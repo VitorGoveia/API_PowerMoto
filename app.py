80e42f41423d71ba4ecc7f834719606c41a4e0d8
João Vitor de Matos Goveia <Vitorgoveia9381@gmail.com>
2025-05-15 13:04:21 -0300

    [feat] Método PUT

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 dici = {
-    "Itens": [{"id": 1,"nome": "arruela"}],
-    "Clientes": [{"telefone": "11 97252-9448", "nome": "Vitor"}],  
+    "Itens": [{"SKU": 1,"nome": "arruela"}],
+    "Clientes": [{"telefone": "11972529448", "nome": "Vitor"}],  
     "Itens_Pedido":[{"id": 1, "SKU_item": "A123", "quantidade": 2}],
     "Pedidos":[{"id": 1, "telefone_cliente": "11 97252-9448", "id_item_pedido": 1}]
 }
@@ -19,10 +19,21 @@ def get_itens():
 @app.route('/itens', methods=['POST'])
 def post_item():
     """Cadastra um item"""
-    novo_item = {"id": 2,"nome": "parafuso"}
+    novo_item = request.json
     dados_itens = dici["Itens"]
     dados_itens.append(novo_item)
-    return jsonify(dados_itens)
+    return jsonify("Item cadastrado com sucesso")
+
+@app.route('/itens/<int:sku_item>', methods=['PUT'])
+def put_item(sku_item):
+    """Alterar informações do item"""
+    itens = dici["Itens"]
+    for item in itens:
+        if item["SKU"] == sku_item:
+            novo_item = request.json
+            item["nome"] = novo_item["nome"]
+            return jsonify("Alteração feita")   
+        return jsonify("SKU do item, não encontrado  ")
 
 #CLIENTES:
 @app.route('/clientes', methods=['GET'])
@@ -34,11 +45,22 @@ def get_clientes():
 @app.route('/clientes', methods=['POST'])
 def post_cliente():
     """Cadastra um cliente"""
-    novo_cliente = {"telefone": "11 94724-9288", "nome": "Miguel"}
+    novo_cliente = request.json
     dados_clientes= dici["Clientes"]
     dados_clientes.append(novo_cliente)
-    return jsonify(dados_clientes)
+    return jsonify("Cliente cadastrado com sucesso")
 
+@app.route('/clientes/<int:numero_cliente>', methods=['PUT'])
+def put_clientes(numero_cliente):
+    """Alterar informaçôes dos clientes"""
+    clientes = dici["Clientes"]
+    for cliente in clientes:
+        if cliente["telefone"] == numero_cliente:
+            novo_cliente = request.json
+            cliente["nome"] = novo_cliente["nome"]
+            return jsonify("Alterações feitas com sucesso")
+        return jsonify("Cliente não encontrado")
+    
 #ITENS PEDIDO:
 @app.route('/itensPedido', methods=['GET'])
 def get_itens_pedido():
@@ -49,11 +71,22 @@ def get_itens_pedido():
 @app.route('/itensPedido', methods=['POST'])
 def post_item_pedido():
     """Cadastra um item do pedido"""
-    novo_item_pedido = {"id": 2, "SKU_item": "B123", "quantidade": 1}
+    novo_item_pedido = request.json
     dados_itens_pedido = dici["Itens_Pedido"]
     dados_itens_pedido.append(novo_item_pedido)
-    return jsonify(dados_itens_pedido)
+    return jsonify("Item do Pedido cadastrado com sucesso")
 
+@app.route('/itensPedido/<int:id_item_pedido>', methods=['PUT'])
+def put_item_pedido(id_item_pedido):
+    itens_pedido = dici["Itens_Pedido"]
+    for item_pedido in itens_pedido:
+        if item_pedido["id"] == id_item_pedido:
+            novo_item_pedido = request.json
+            item_pedido["SKU_item"] = novo_item_pedido["SKU_item"]
+            item_pedido["quantidade"] = novo_item_pedido["quantidade"]
+            return jsonify("Alteração feita com sucesso")
+        return jsonify("Item de pedido não encontrado")
+    
 #ORÇAMENTOS:
 @app.route('/pedidos', methods=['GET'])
 def get_pedidos():
@@ -64,10 +97,22 @@ def get_pedidos():
 @app.route('/pedidos', methods=['POST'])
 def post_pedido():
     """Cadastrar pedido"""
-    novo_pedido = {"id": 2, "telefone_cliente": "11 97252-9448", "id_item_pedido": [1,2]}
+    novo_pedido = request.json
     dados_pedidos = dici["Pedidos"]
     dados_pedidos.append(novo_pedido)
-    return jsonify(dados_pedidos)
+    return jsonify("Pedido cadastrado com sucesso")
+
+@app.route('/pedidos/<int:id_pedido>', methods=['PUT'])
+def put_pedido(id_pedido):
+    """Alterar dados do pedido"""
+    pedidos = dici["Pedidos"]
+    for pedido in pedidos:
+        if pedido["id"] == id_pedido:
+            novo_pedido = request.json
+            pedido["telefone_cliente"] = novo_pedido["telefone_cliente"]
+            pedido["id_item_pedido"] = novo_pedido["id_item_pedido"]
+            return jsonify("Alteração feita com sucesso")
+        return jsonify("Pedido não encontrado")
 
 if __name__ == '__main__':
     app.run(debug=True)
