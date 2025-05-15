28455eaf9c8e6fc326579a3fc25a34e17fd97398
João Vitor de Matos Goveia <Vitorgoveia9381@gmail.com>
2025-05-15 14:20:27 -0300

    [feat] Método DELETE

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 dici = {
-    "Itens": [{"SKU": 1,"nome": "arruela"}],
+    "Itens": [{"SKU": "A123","nome": "arruela", "valor": 1.2, "marca": "Bajaj"}],
     "Clientes": [{"telefone": "11972529448", "nome": "Vitor"}],  
-    "Itens_Pedido":[{"id": 1, "SKU_item": "A123", "quantidade": 2}],
+    "Itens_Pedido":[{"id": 1, "SKU_item": "A123", "quantidade": 2, "prazo": 110}],
     "Pedidos":[{"id": 1, "telefone_cliente": "11 97252-9448", "id_item_pedido": 1}]
 }
 
@@ -24,7 +24,7 @@ def post_item():
     dados_itens.append(novo_item)
     return jsonify("Item cadastrado com sucesso")
 
-@app.route('/itens/<int:sku_item>', methods=['PUT'])
+@app.route('/itens/<string:sku_item>', methods=['PUT'])
 def put_item(sku_item):
     """Alterar informações do item"""
     itens = dici["Itens"]
@@ -32,9 +32,22 @@ def put_item(sku_item):
         if item["SKU"] == sku_item:
             novo_item = request.json
             item["nome"] = novo_item["nome"]
+            item["valor"] = novo_item["valor"]
+            item["marca"] = novo_item["marca"]
             return jsonify("Alteração feita")   
-        return jsonify("SKU do item, não encontrado  ")
+    return jsonify("SKU do item, não encontrado  ")
 
+@app.route('/itens/<string:sku_item>', methods=['DELETE'])
+def deletar_item(sku_item):
+    """Deleta item registrado"""
+    itens = dici["Itens"]
+    for item in itens:
+        if item["SKU"] == sku_item:
+            item_deletado = request.json
+            itens.remove(item)
+            return jsonify(item_deletado)
+    return jsonify("Item não encontrado")
+    
 #CLIENTES:
 @app.route('/clientes', methods=['GET'])
 def get_clientes():
@@ -50,7 +63,7 @@ def post_cliente():
     dados_clientes.append(novo_cliente)
     return jsonify("Cliente cadastrado com sucesso")
 
-@app.route('/clientes/<int:numero_cliente>', methods=['PUT'])
+@app.route('/clientes/<string:numero_cliente>', methods=['PUT'])
 def put_clientes(numero_cliente):
     """Alterar informaçôes dos clientes"""
     clientes = dici["Clientes"]
@@ -59,7 +72,17 @@ def put_clientes(numero_cliente):
             novo_cliente = request.json
             cliente["nome"] = novo_cliente["nome"]
             return jsonify("Alterações feitas com sucesso")
-        return jsonify("Cliente não encontrado")
+    return jsonify("Cliente não encontrado")
+
+@app.route('/clientes/<string:numero_cliente>', methods=['DELETE'])
+def delete_clientes(numero_cliente):
+    clientes = dici["Clientes"]
+    for cliente in clientes:
+        if cliente["telefone"] == numero_cliente:
+            cliente_deletado = request.json
+            clientes.remove(cliente)
+            return jsonify(cliente_deletado)
+    return jsonify('Cliente não encontrado')
     
 #ITENS PEDIDO:
 @app.route('/itensPedido', methods=['GET'])
@@ -84,9 +107,22 @@ def put_item_pedido(id_item_pedido):
             novo_item_pedido = request.json
             item_pedido["SKU_item"] = novo_item_pedido["SKU_item"]
             item_pedido["quantidade"] = novo_item_pedido["quantidade"]
+            item_pedido["prazo"] = novo_item_pedido["prazo"]
+
             return jsonify("Alteração feita com sucesso")
-        return jsonify("Item de pedido não encontrado")
-    
+    return jsonify("Item de pedido não encontrado")
+
+@app.route('/itensPedido/<int:id_item_pedido>', methods=['DELETE'])
+def delete_item_pedido(id_item_pedido):
+    """Deleta item do pedido cadastrado"""
+    itens_pedido = dici["Itens_Pedido"]
+    for item_pedido in itens_pedido:
+        if itens_pedido["id"] == id_item_pedido:
+            item_pedido_deletado = request.json
+            itens_pedido.remove(item_pedido)
+            return jsonify(item_pedido_deletado)
+    return jsonify("Item do pedido não encontrado")
+
 #ORÇAMENTOS:
 @app.route('/pedidos', methods=['GET'])
 def get_pedidos():
@@ -112,7 +148,17 @@ def put_pedido(id_pedido):
             pedido["telefone_cliente"] = novo_pedido["telefone_cliente"]
             pedido["id_item_pedido"] = novo_pedido["id_item_pedido"]
             return jsonify("Alteração feita com sucesso")
-        return jsonify("Pedido não encontrado")
+    return jsonify("Pedido não encontrado")
+
+@app.route('/pedidos/<int:id_pedido>', methods=['DELETE'])
+def deletar_pedido(id_pedido):
+    pedidos = dici["Pedidos"]
+    for pedido in pedidos:
+        if pedido["id"] == id_pedido:
+            pedido_deletado = request.json
+            pedidos.remove(pedido)
+            return jsonify(pedido_deletado)
+    return jsonify("Pedido não encontrado")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
